653268c58aa31bdf4457f1341e4fda6c0bfb0c1d
ygtzz <ygtzz@126.com>
2016-06-06 15:05:45 +0800

    add fis

---
+++ fis-conf.js
@@ -0,0 +1,10 @@
+// 清除其他配置，只保留如下配置
+fis.match('*.js', {
+  // fis-optimizer-uglify-js 插件进行压缩，已内置
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+  // fis-optimizer-clean-css 插件进行压缩，已内置
+  optimizer: fis.plugin('clean-css')
+});
