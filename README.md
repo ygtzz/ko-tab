3c282b4fcbcfe26519150205bed70311563a3f96
ygtzz <ygtzz@126.com>
2016-06-06 15:26:11 +0800

    add min version

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ko-tab
+压缩文件 fis3 release -d ./dist
--- fis-conf.js
+++ fis-conf.js
@@ -1,10 +1,15 @@
+fis.match('*',{
+	release:false
+});
 // 清除其他配置，只保留如下配置
-fis.match('*.js', {
+fis.match('(ko-tab).js', {
   // fis-optimizer-uglify-js 插件进行压缩，已内置
-  optimizer: fis.plugin('uglify-js')
+  optimizer: fis.plugin('uglify-js'),
+  release:'$1.min.js'
 });
 
-fis.match('*.css', {
+fis.match('(ko-tab).css', {
   // fis-optimizer-clean-css 插件进行压缩，已内置
-  optimizer: fis.plugin('clean-css')
+  optimizer: fis.plugin('clean-css'),
+  release:'$1.min.css'
 });
